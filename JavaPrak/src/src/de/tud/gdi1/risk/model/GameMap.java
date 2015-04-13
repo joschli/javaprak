984f980fef2d67dd50a467cc90718da746682fbf
Tim Witzel <timwitzel94@web.de>
2015-04-14 01:17:32 +0200

    game map small change

--- JavaPrak/src/src/de/tud/gdi1/risk/model/GameMap.java
+++ JavaPrak/src/src/de/tud/gdi1/risk/model/GameMap.java
@@ -1,4 +1,5 @@
 package src.de.tud.gdi1.risk.model;
+
 import java.util.ArrayList;
 
 import org.newdawn.slick.Color;
@@ -6,7 +7,6 @@ import org.newdawn.slick.geom.Vector2f;
 
 import src.de.tud.gd1.risk.factory.CountryFactory;
 import src.de.tud.gdi1.risk.model.entities.Country;
-
 
 public class GameMap {
 
@@ -16,18 +16,17 @@ public class GameMap {
 	private Mission[] missions;
 	private ArrayList<Country> countries = new ArrayList<Country>();
 	private ArrayList<Card> cards = new ArrayList<Card>();
-	
-	//Constructor for loading a map from a txt file
-	public GameMap(String path){
+
+	// Constructor for loading a map from a txt file
+	public GameMap(String path) {
 		loadMap(path);
 	}
-	
-	public GameMap(Player[] players)
-	{
+
+	public GameMap(Player[] players) {
 		this.continents = new ArrayList<Continent>();
 		this.players = new Player[players.length];
-		this.players =	players;
-		
+		this.players = players;
+
 		init();
 		assignCountries();
 		initPossibleMissions();
@@ -35,44 +34,40 @@ public class GameMap {
 		createReinforcements();
 		colorizeCountries();
 	}
-	
+
 	private void colorizeCountries() {
-		for(Continent conti : continents)
-		{
-			for(int index : conti.getCountries())
-			{
+		for (Continent conti : continents) {
+			for (int index : conti.getCountries()) {
 				countries.get(index).setColor(conti.getColor());
 			}
 		}
 	}
 
 	private void createReinforcements() {
-		for(Player p : players)
-		{
-			p.addReinforcement((2 * getCountries().size())/players.length);	
-				
+		for (Player p : players) {
+			p.addReinforcement((2 * getCountries().size()) / players.length);
+
 		}
 	}
 
-	//loads a Map txt-file and creates the continents and countries for it
-	private static void loadMap(String path){
-		
+	// loads a Map txt-file and creates the continents and countries for it
+	private static void loadMap(String path) {
+
 	}
-	
-	public void saveMap(String path){
-		
+
+	public void saveMap(String path) {
+
 	}
-	
-	public void init()
-	{
+
+	public void init() {
 		Continent A = new Continent(2, Color.cyan);
 		Continent B = new Continent(1, Color.magenta);
 		Continent C = new Continent(3, Color.yellow);
-		countryFactory = new CountryFactory("a", 1, new Vector2f(200,200));
+		countryFactory = new CountryFactory("a", 1, new Vector2f(200, 200));
 		Country a = (Country) countryFactory.createEntity();
 		A.addCountry(0);
 		countries.add(a);
-		countryFactory.updateFactory("b", 1, new Vector2f(200,360));
+		countryFactory.updateFactory("b", 1, new Vector2f(200, 360));
 		Country b = (Country) countryFactory.createEntity();
 		B.addCountry(1);
 		countries.add(b);
@@ -80,126 +75,112 @@ public class GameMap {
 		Country c = (Country) countryFactory.createEntity();
 		C.addCountry(2);
 		countries.add(c);
-		countryFactory.updateFactory("d", 10, new Vector2f(360,360));
+		countryFactory.updateFactory("d", 10, new Vector2f(360, 360));
 		Country d = (Country) countryFactory.createEntity();
 		C.addCountry(3);
 		countries.add(d);
-		
+
 		continents.add(A);
 		continents.add(B);
 		continents.add(C);
 	}
-	
-	public void init(String path)
-	{
-		
+
+	public void init(String path) {
+
 	}
-	
-	public ArrayList<Country> getCountries()
-	{
+
+	public ArrayList<Country> getCountries() {
 		return countries;
 	}
-	
-	public ArrayList<Continent> getContinents()
-	{
+
+	public ArrayList<Continent> getContinents() {
 		return continents;
-		
+
 	}
-	
-	public void setCountries(ArrayList<Country> c)
-	{
+
+	public void setCountries(ArrayList<Country> c) {
 		countries = c;
 	}
-	
-	public Player[] getPlayers()
-	{
+
+	public Player[] getPlayers() {
 		return players;
 	}
-	
-	public Player getPlayer(int index)
-	{
+
+	public Player getPlayer(int index) {
 		return players[index];
 	}
-	
-	public int getOwnedCountriesForPlayer(int index)
-	{
+
+	public int getOwnedCountriesForPlayer(int index) {
 		int count = 0;
-		
-		for(Country c : getCountries())
-		{
-			if(c.isOwner(players[index]))
+
+		for (Country c : getCountries()) {
+			if (c.isOwner(players[index]))
 				count++;
 		}
-		
+
 		return count;
-		
+
 	}
-	
-	
-	
+
 	private void assignCountries() {
-		
+
 		int index = 0;
 		int playerIndex = 0;
-		while(index < countries.size())
-		{
+		while (index < countries.size()) {
 			int random = (int) (Math.random() * countries.size());
-			while(countries.get(random).hasOwner())
-			{
+			while (countries.get(random).hasOwner()) {
 				random = (int) (Math.random() * countries.size());
 			}
 			countries.get(random).setOwner(players[playerIndex]);
 			countries.get(random).addTroops(1);
-			playerIndex = (playerIndex == players.length-1) ? 0 : playerIndex+1;
+			playerIndex = (playerIndex == players.length - 1) ? 0
+					: playerIndex + 1;
 			index++;
 		}
-		
-		for(int i = 0; i < countries.size(); ++i)
-		{
-			System.out.println("Country: " + countries.get(i).getName() + " | Owner: " + countries.get(i).getOwner().getName());
+
+		for (int i = 0; i < countries.size(); ++i) {
+			System.out.println("Country: " + countries.get(i).getName()
+					+ " | Owner: " + countries.get(i).getOwner().getName());
 		}
 	}
-	
-	private void initPossibleMissions()
-	{
+
+	private void initPossibleMissions() {
 		missions = new Mission[10];
-		//String missionText, Player player,
-		//ArrayList<Continent> continents, int countryCount
+		// String missionText, Player player,
+		// ArrayList<Continent> continents, int countryCount
 		missions[0] = new Mission("Defeat Player 0", players[0], null, -1);
 		missions[1] = new Mission("Defeat Player 1", players[1], null, -1);
 		ArrayList<Continent> con = new ArrayList<Continent>();
 		con.add(continents.get(0));
 		con.add(continents.get(2));
-		missions[2] = new Mission("Conquer the following Continents: A,C",null , con, -1);
+		missions[2] = new Mission("Conquer the following Continents: A,C",
+				null, con, -1);
 		con.clear();
 		con.add(continents.get(0));
 		con.add(continents.get(1));
-		missions[3] = new Mission("Conquer the following Continents: A,B", null, con, -1);
+		missions[3] = new Mission("Conquer the following Continents: A,B",
+				null, con, -1);
 		missions[4] = new Mission("Conquer 4 Countries", null, null, 4);
 		/*
-		missions[5] = new Mission("Defeat Player 0", players[0], null, -1);
-		missions[6] = new Mission("Defeat Player 0", players[0], null, -1);
-		missions[7] = new Mission("Defeat Player 0", players[0], null, -1);
-		missions[8] = new Mission("Defeat Player 0", players[0], null, -1);
-		missions[9] = new Mission("Defeat Player 0", players[0], null, -1);
-		*/
+		 * missions[5] = new Mission("Defeat Player 0", players[0], null, -1);
+		 * missions[6] = new Mission("Defeat Player 0", players[0], null, -1);
+		 * missions[7] = new Mission("Defeat Player 0", players[0], null, -1);
+		 * missions[8] = new Mission("Defeat Player 0", players[0], null, -1);
+		 * missions[9] = new Mission("Defeat Player 0", players[0], null, -1);
+		 */
 		boolean[] taken = new boolean[10];
-		for(Player p: players)
-		{
+		for (Player p : players) {
 			int random = (int) (Math.random() * missions.length);
 			while (taken[random])
-				 random = (int) (Math.random() * missions.length);
+				random = (int) (Math.random() * missions.length);
 			p.assignMission(missions[random]);
-			taken[random] = true;	
+			taken[random] = true;
 		}
-		
+
 	}
 
-
-
 	private void createCards() {
-		for(Country c : countries)
-		{
+		for (Country c : countries) {
 			cards.add(new Card(c, c.getCardValue()));
 		}
 	}
